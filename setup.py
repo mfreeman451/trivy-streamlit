823923ded1119dea326b82bc31f0be59a65f91c3
Michael Freeman <mfreeman451@gmail.com>
2024-01-18 23:41:06 -0600

    ✨ pypi stuff

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trivy-streamlit',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit',
+    ],
+    entry_points={
+        'console_scripts': [
+            'trivy-streamlit=trivy_streamlit.main_module:start_app',
+        ],
+    },
+    # Additional metadata
+    author='Michael Freeman',
+    author_email='mfreeman451@gmail.com',
+    description='A simple streamlit app for trivy security scanner JSON output',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mfreeman451/trivy-streamlit',
+    license='Apache-2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Security',
+    ],
+)
+
